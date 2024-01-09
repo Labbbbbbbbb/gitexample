8e6d9fab50c79e634c5503a72edaad896f11165b
labbbbbbbbb <724757025@qq.com>
2024-01-09 01:37:42 -0600

    init commit

---
+++ test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    printf("123\n");
+    
+    printf("abc\n");
+    printf("嘻嘻\n");
+    int a=0;
+    scanf("%d",&a);
+    printf("a=%d",a);
+
+    return 0;
+}
